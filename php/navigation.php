a04fec728e4424a7fb5f16ad10fc6c5586dde5e0
KavishkaSashmitha <kavivlogs20@gmail.com>
2023-12-10 22:02:10 +0530

    Main Ui complete

---
+++ php/navigation.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chase Landing Layout</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/featherlight.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+	
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script type="text/javascript" src="js/featherlight.min.js"></script> 
+
+</head>
+<body>
+	<header id="top">
+		<div class="wrapper">
+			<h1 class="logo"><a href="#">Chase</a></h1>
+			<nav>
+				<ul id="navigation">
+					<li><a href="#home">Home</a></li>
+					<li><a href="#about">About Us</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a class="nav-cta" href="#">Sign In</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
---
+++ php/signin.php
@@ -0,0 +1,5 @@
+<?php
+    require"navigation.php";
+
+?>
+
